cfba5ca3c7dc86b361247bb7728c8721fcdfc3ca
m3hr4d <47602731+m3hr4d@users.noreply.github.com>
2025-07-03 21:31:34 +0330

    chore: enable strict type checking

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,10 +8,23 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "next/core-web-vitals",
+    "next/typescript"
+  ),
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
 ];
 
 export default eslintConfig;
